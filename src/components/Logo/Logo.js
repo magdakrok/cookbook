7e6eab6964cd2b2815421707addffc52c0d1a145
magda.krok <magdalena.krok@interia.pl>
2020-10-27 15:53:24 +0100

    add Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import cakeLogo from '../../assets/images/cake128px.png';
+import classes from './Logo.module.css';
+
+const logo = (props) =>(
+    <div className={classes.Logo}>
+        <img src={cakeLogo} alt="My cake logo"/>
+    </div>
+);
+
+export default logo;

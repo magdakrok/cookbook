56b4b86f84b4b6e09c9b3d177b80378f0d8200c7
magda.krok <magdalena.krok@interia.pl>
2020-10-28 13:54:20 +0100

    add backdrop to sideDrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,22 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/auxiliary';
 import classes from './SideDrawer.module.css';
 
 const sideDrawer = (props) => {
 
     return(
+        <Aux>
+            <Backdrop show/>
         <div className={classes.SideDrawer}>
             <Logo  height="11%"/>
             <nav>
                 <NavigationItems />
             </nav>
         </div>
+        </Aux>
     );
 }
 

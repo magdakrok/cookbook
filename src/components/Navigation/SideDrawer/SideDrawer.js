aca2e1a70e768fe6d12a111f6c7ec05ea642bf16
magda.krok <magdalena.krok@interia.pl>
2020-10-29 13:15:45 +0100

    add logic to backdrop component

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,11 +7,15 @@ import Aux from '../../../hoc/auxiliary';
 import classes from './SideDrawer.module.css';
 
 const sideDrawer = (props) => {
-
+ 
+    let attachedClasses = [classes.SideDrawer, classes.Close]
+    if(props.open){
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return(
         <Aux>
-            <Backdrop show/>
-        <div className={classes.SideDrawer}>
+            <Backdrop show={props.open} clicked={props.closed}/>
+        <div className={attachedClasses.join(' ')}>
             <Logo  height="11%"/>
             <nav>
                 <NavigationItems />

88345416461880888d00ad6c7c63d49b65f7448b
magda.krok <magdalena.krok@interia.pl>
2020-11-05 15:44:06 +0100

    mapping fetch data

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary/auxiliary';
+import classes from './Recipe.module.css';
 
 const recipe = (props) =>{
 
     const data = props.data;
-
-     let transformedData = data.map(({title, http}) => {
-         return <p key={title}>{title} - {http}</p>
-     });
-  
+     
+   let transformedData = Object.keys(data).map(key => {
+      return [...Array(data[key])].map(cKey => {
+          return <p key={cKey}>{cKey.title} - {cKey.http}</p>
+        
+      })
+  })
+   
        console.log(transformedData);
 
     if(transformedData.length === 0){
@@ -18,7 +22,9 @@ const recipe = (props) =>{
    
         return(
             <Aux>
+                <div className={classes.Recipe}>
                 {transformedData}
+                </div>
                 <div>Build Controls</div>
             </Aux>
         );
--- src/containers/RecipesBuilder/RecipesBuilder.js
+++ src/containers/RecipesBuilder/RecipesBuilder.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import Recipe from '../../components/Recipe/Recipe';
 import Aux from '../../hoc/Auxiliary/auxiliary';
+import axios from '../../axios-instance';
 
 class RecipesBuilder extends Component{
 
     state = {
-        data: [{
-            http: 'http://przepisy.pl',
-            photo: 'http://przepisy.pl/photo',
-            title: 'ciasteczka'},
-    {
-        http: 'http://przyslijprzepis.pl',
-        photo: 'http://przepisy.pl/photo',
-        title: 'Mazurek'},
-        
-]}
+        cake: null
+    }
+
+componentDidMount(){
+    axios.get('https://cookbook-addec.firebaseio.com/cake.json')
+    .then(response => {
+        this.setState({cake: response.data});
+        console.log(response);
+
+       
+    })
+}
+
+
+
 
     render(){
+        
+   let recipe;
+
+        if(this.state.cake){
+       recipe = (<Recipe data={this.state.cake}/>)
+        }else{
+            recipe = <p>Loading...</p>
+        }
         return(
             <Aux>
-                <Recipe data={this.state.data}/>
+                
+                {recipe}
                 <div>Build Controls</div>
             </Aux>
         );

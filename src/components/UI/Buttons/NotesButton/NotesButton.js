8aefdb4b73ad9fbe5fa98b907fadd5ef3afeb13a
magda.krok <magdalena.krok@interia.pl>
2020-12-02 12:47:33 +0100

    add NotesButton

---
+++ src/components/UI/Buttons/NotesButton/NotesButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './NotesButton.module.css';
+
+const NotesButton = () => {
+    return(
+        <button className={classes.NotesButton}></button>
+    )
+}
+
+export default NotesButton;

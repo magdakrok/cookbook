c69b0479d871d4ecaf7f8aa4b2021b32611b1098
magda.krok <magdalena.krok@interia.pl>
2021-02-02 15:14:50 +0100

    add the picute to homepage and change the toggle color, logo anand toolbar

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,9 +6,9 @@ import {Link } from 'react-router-dom';
 const homePage = (props) => (
 
     <div className={classes.HomePage}>
-        <p>strona główna</p>
+       <div className={classes.Cake}></div>
         <ul>
-            <li><Link to="/cake">Ciasta</Link></li>
+        <div><li><Link to="/cake">Ciasta</Link></li></div>
         
         <div><li><Link to="/salad">Sałatki</Link></li></div>
         <div><li><Link to="/dish">Dania główne</Link></li>

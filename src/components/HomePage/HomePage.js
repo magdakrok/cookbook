32f2f1025cae088df98db381f68401be76fa6c00
magda.krok <magdalena.krok@interia.pl>
2021-02-02 17:50:10 +0100

    add the text and style to homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,7 +6,13 @@ import {Link } from 'react-router-dom';
 const homePage = (props) => (
 
     <div className={classes.HomePage}>
-       <div className={classes.Cake}></div>
+       <div className={classes.Cake}>
+           <div className={classes.Text}>
+                <h3>Twoja</h3> 
+                <h1>Książka Kucharska</h1>
+                <h3>Zbiór najlepszych przepisów z Internetu</h3>
+          </div>
+       </div>
         <ul>
         <div><li><Link to="/cake">Ciasta</Link></li></div>
         
